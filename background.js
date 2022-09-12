ad1746ea54d1da96bcf4adaa60e188b32a7043ff
luoxupan <luoxupan@didiglobal.com>
2022-09-12 18:42:08 +0800

    save

--- background.js
+++ background.js
@@ -1,7 +1,15 @@
-let color = '#3aa757';
+// let color = '#3aa757';
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
-});
+// chrome.runtime.onInstalled.addListener(() => {
+//   chrome.storage.sync.set({ color });
+//   console.log('Default background color set to %cgreen', `color: ${color}`);
+// });
 
+chrome.webRequest.onBeforeRedirect.addListener(
+  function(details) {
+    console.log('details:',details)
+    'data:text/html;charset=utf-8,dddddd'
+  },
+  {urls: ["<all_urls>"]},
+  ["responseHeaders"]
+);
--- js/popup.js
+++ js/popup.js
@@ -1,22 +1,19 @@
-let changeColor = document.querySelector("#changeColor");
-
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.color = color;
-});
-
-changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    func: setPageBackgroundColor,
-  });
-});
+// let changeColor = document.querySelector("#changeColor");
+// chrome.storage.sync.get("color", ({ color }) => {
+//   changeColor.style.color = color;
+// });
+// changeColor.addEventListener("click", async () => {
+//   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+//   chrome.scripting.executeScript({
+//     target: { tabId: tab.id },
+//     func: setPageBackgroundColor,
+//   });
+// });
 
 // The body of this function will be executed as a content script inside the
 // current page
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
-  });
-}
+// function setPageBackgroundColor() {
+//   chrome.storage.sync.get("color", ({ color }) => {
+//     document.body.style.backgroundColor = color;
+//   });
+// }
