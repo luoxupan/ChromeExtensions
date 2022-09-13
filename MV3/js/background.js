f235e3eca1e33b63aa2388c56c524ebe2212d3bf
luoxupan <luoxupan@didiglobal.com>
2022-09-13 18:16:46 +0800

    save

--- MV3/js/background.js
+++ MV3/js/background.js
@@ -5,11 +5,78 @@
 //   console.log('Default background color set to %cgreen', `color: ${color}`);
 // });
 
-chrome.webRequest.onBeforeRedirect.addListener(
-  function(details) {
-    console.log('details:',details)
-    'data:text/html;charset=utf-8,dddddd'
+// chrome.webRequest.onBeforeRedirect.addListener(
+//   function(details) {
+//     console.log('details:',details)
+//     'data:text/html;charset=utf-8,dddddd'
+//   },
+//   {urls: ["<all_urls>"]},
+//   ["responseHeaders"]
+// );
+
+const RulesTemp = {
+  ResProxy(id, redirect_url, RegExp_url) {
+    return {
+      "id": id,
+      "priority": 1,
+      "action": {
+        "type": "redirect",
+        "redirect": { "url": redirect_url }
+      },
+      "condition": {
+        "urlFilter": RegExp_url,
+        "resourceTypes": ["script", "xmlhttprequest"]
+      }
+    }
   },
-  {urls: ["<all_urls>"]},
-  ["responseHeaders"]
-);
+};
+
+function generatorRules(existRules, rules) {
+  let addRules = [];
+  let removeRuleIds = [];
+  let enableRulesetIds = [];
+  let disableRulesetIds = [];
+  for (let i = 0; i < rules.length; ++i) {
+    const { RegExp_url, redirect_url, enabled, type, id } = rules[i];
+    const isExist = existRules.includes(id);
+    if (enabled) {
+      if (isExist) {
+        enableRulesetIds.push(String(id));
+      } else if (type && RegExp_url && redirect_url) {
+        removeRuleIds.push(id);
+        addRules.push(RulesTemp[type](id, redirect_url, RegExp_url));
+      }
+    }
+    if (!enabled && isExist) {
+      disableRulesetIds.push(String(id));
+    }
+  }
+  return {
+    addRules, removeRuleIds,
+    enableRulesetIds, disableRulesetIds
+  };
+}
+
+/**
+ * onChanged监听数据的格式
+ * { "rules": { "newValue": {}, "oldValue": {} } }
+ */
+chrome.storage.onChanged.addListener(async function (changes, namespace) {
+  const { rules } = await chrome.storage.sync.get(['rules']);
+  console.log('rules is:' + JSON.stringify(rules));
+
+  const existRules = await chrome.declarativeNetRequest.getDynamicRules();
+  console.log('existRules:', existRules)
+
+  const {
+    addRules, removeRuleIds,
+    disableRulesetIds, enableRulesetIds
+  } = generatorRules(existRules.map(({ id }) => id), rules);
+  console.log('rules:=', {addRules, removeRuleIds, disableRulesetIds, enableRulesetIds} )
+  /**
+   * 文档
+   * https://developer.chrome.com/docs/extensions/reference/declarativeNetRequest/#method-updateDynamicRules
+   */
+  chrome.declarativeNetRequest.updateDynamicRules({ addRules, removeRuleIds });
+  chrome.declarativeNetRequest.updateEnabledRulesets({ disableRulesetIds, enableRulesetIds });
+});
--- MV3/js/options.js
+++ MV3/js/options.js
@@ -1,1 +1,41 @@
+function uuid() {
+  return (Math.random() * 10241024) | 0 | 0x8;
+}
 
+let rules = [
+	{
+		"RegExp_url": "https://luoxupan.github.io/wiki/pages/webapp/main_04b9bf45.js",
+		"redirect_url": "http://localhost:8080/main_04b9bf45.js",
+		"enabled": true,
+    "type": "ResProxy",
+    "id": 1
+	},
+	{
+		"RegExp_url": "https://cstest.xxxxx.com/cs-ticket-system/*",
+		"redirect_url": "http://localhost:3000/",
+		"enabled": true,
+    "type": "ResProxy",
+    "id": 2
+	},
+	{
+		"RegExp_url": "https://cstest.xxxxx.com/static/js/*",
+		"redirect_url": "http://localhost:3000/static/js/",
+		"enabled": false,
+    "type": "ResProxy",
+    "id": 3
+	},
+	{
+		"RegExp_url": "https://cstest.xxxxx.com/static/kfcommon/*",
+		"redirect_url": "http://localhost:8686/static/kfcommon/",
+		"enabled": false,
+    "type": "ResProxy",
+    "id": 4
+	}
+];
+
+/**
+ * 文档
+ * https://developer.chrome.com/docs/extensions/reference/storage/
+ */
+
+chrome.storage.sync.set({ rules: rules });
