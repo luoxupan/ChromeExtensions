c0314b4783f53f17ca31d3b45f96cacc2178e013
luoxupan <luoxupan@didiglobal.com>
2022-09-15 14:55:29 +0800

    save

--- js/background.js
+++ js/background.js
@@ -1,19 +1,3 @@
-// let color = '#3aa757';
-
-// chrome.runtime.onInstalled.addListener(() => {
-//   chrome.storage.sync.set({ color });
-//   console.log('Default background color set to %cgreen', `color: ${color}`);
-// });
-
-// chrome.webRequest.onBeforeRedirect.addListener(
-//   function(details) {
-//     console.log('details:',details)
-//     'data:text/html;charset=utf-8,dddddd'
-//   },
-//   {urls: ["<all_urls>"]},
-//   ["responseHeaders"]
-// );
-
 const RulesTemp = {
   ResProxy(id, redirect_url, RegExp_url) {
     return {
